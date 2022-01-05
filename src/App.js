6cb5fef20e337b041c70038179295d44adbf8a1a
babakdorani <babak.dorani@gmail.com>
2022-01-05 23:26:52 +0200

    all files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+
+const languages = [
+  {
+    code: "fr",
+    name: "français",
+    country_code: "fr",
+    dir: "lrt",
+  },
+  {
+    code: "en",
+    name: "English",
+    country_code: "gb",
+    dir: "lrt",
+  },
+  {
+    code: "ar",
+    name: "العربیه",
+    country_code: "sa",
+    dir: "rtl",
+  },
+];
 function App() {
+  const currentLanguageCode = Cookies.get("i18next") || "en";
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode);
+  const { t } = useTranslation();
+  const releaseDate = new Date("2021-03-07");
+  const timeDifference = new Date() - releaseDate;
+  const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  useEffect(() => {
+    document.body.dir = currentLanguage.dir;
+    document.title = t("app_title");
+  }, [currentLanguage, t]);
   return (
-    <div className="App">
-      Hello World
+    <div className="container">
+      <div className="d-flex justify-content-end">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            choose language
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            {languages.map(({ code, name, country_code }) => (
+              <li key={country_code}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => i18next.changeLanguage(code)}
+                  disabled={code === currentLanguage.code}
+                >
+                  <span
+                    className={`flag-icon flag-icon-${country_code} mx-2`}
+                  ></span>
+                  {name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="font-weight-normal mb-3">{t("welcome_message")}</h1>
+        <p>{t("days_since_release", { number_of_days })}</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "flag-icon-css/css/flag-icons.min.css";
+import "./index.css";
+import App from "./App";
 
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "fr", "ar"],
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+
+    // lng: document.querySelector("html").lang, // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+    detection: {
+      order: [
+        "cookie",
+        "htmlTag",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "path",
+        "subdomain",
+        "querystring",
+      ],
+      caches: ["cookie"],
+    },
+    backend: {
+      // we have to say where is our locales
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  });
+
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h2>Loading...</h2>
+  </div>
+);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+  document.getElementById("root")
 );
